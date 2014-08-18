bfaba02b632842cd06d9ba758b754ae677863d46
Thomas Junier <thomas.junier@epfl.ch>
2014-08-18 17:36:19 +0200

    NewickParser parses String, but produces Data.Tree Data.Text

--- src/NewickParser.hs
+++ src/NewickParser.hs
@@ -1,5 +1,8 @@
 module NewickParser where
 
+-- Hm, for now it will parse String, not Data.Text (but the trees should be very
+-- small compared to the alignments, so it should make no difference).
+
 import Text.ParserCombinators.Parsec
 import qualified Data.Text as T
 import Data.Tree
@@ -20,26 +23,26 @@ semicolon = char ';' >> return ()
 comma :: Parser ()
 comma = char ',' >> return ()
 
-nwLeaf :: Parser (Tree String)
+nwLeaf :: Parser (Tree T.Text)
 nwLeaf = do
     id <- identifier
-    return $ Node id []
+    return $ Node (T.pack id) []
 
-nwNode :: Parser (Tree String)
+nwNode :: Parser (Tree T.Text)
 nwNode = do	{ oParen
 			  ; children <- nwNode `sepBy` comma
 			  ; cParen
-			  ; return $ Node "" children
+			  ; return $ Node T.empty children
 			}
 		 <|> nwLeaf
 
-newickTree :: Parser (Tree String)
+newickTree :: Parser (Tree T.Text)
 newickTree = do
     tree <- nwNode
     semicolon
     return tree
 
-parseNewickTree :: String -> Either ParseError (Tree String)
+parseNewickTree :: String -> Either ParseError (Tree T.Text)
 parseNewickTree newick = (parse newickTree "" newick)
 
 run :: Show a => Parser a -> String -> IO ()
