18453e6377e35bb53f031ad885e9fd618f168dd8
Thomas Junier <thomas.junier@epfl.ch>
2014-08-20 10:16:21 +0200

    moved fastARecordsToAlnMap to FastA module

--- src/FastA.hs
+++ src/FastA.hs
@@ -1,22 +1,79 @@
 module FastA (FastA(..), fastATextToRecords) where
 
-import qualified Data.Text.Lazy as T
+import qualified Data.Map.Strict as M -- most likely going to use all values
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text as ST
+import qualified Data.List as L
 
-data FastA = FastA { header :: T.Text, sequence :: T.Text } deriving (Show)
+import MlgscTypes
+
+data FastA = FastA { header :: LT.Text, sequence :: LT.Text } deriving (Show)
 
 -- takes a FastA input (one or more records) and returns a list of FastA
 -- records.
 
-fastATextToRecords :: T.Text -> [FastA]
+fastATextToRecords :: LT.Text -> [FastA]
 fastATextToRecords fasta = map chunk2FastA chunks 
-    where chunks = T.splitOn (T.pack "\n>") $ T.tail fasta
+    where chunks = LT.splitOn (LT.pack "\n>") $ LT.tail fasta
 
-chunk2FastA :: T.Text -> FastA
+chunk2FastA :: LT.Text -> FastA
 chunk2FastA chunk = FastA hdr seq
     where   hdr = head chunk_lines
-            seq = T.concat $ tail chunk_lines 
-            chunk_lines = T.lines chunk
+            seq = LT.concat $ tail chunk_lines 
+            chunk_lines = LT.lines chunk
 
+
+fastARecordsToAlnMap :: [FastA] -> M.Map  SciName Alignment
+fastARecordsToAlnMap = undefined
+
+-- Some data to play around with in GHCi
+
+fasta = unlines [
+    ">Genus_A",
+    "AXGCAT",
+    "GCGTGT",
+    ">Genus_B",
+    "BXG",
+    "CAT",
+    "GCG",
+    "TGC",
+    ">Genus_A",
+    "AYGC",
+    "ATGC",
+    "GTGC",
+    ">Genus_C",
+    "CXGCATGCATGC",
+    ">Genus_B",
+    "BYGCATGCATAC",
+    ">Genus_C",
+    "CYGCATGCATGC",
+    ">Genus_C",
+    "CZGCTTGCATGC"
+    ]
+
+fastaRecs = fastATextToRecords $ LT.pack fasta
+
+headers = map (LT.toStrict . header) fastaRecs
+
+-- initial map, with one empty list per genus:
+
+initMap = L.foldl (\map otu -> M.insert otu [] map) M.empty headers
+
+-- how to insert one element into the map, using Cons (:) instead of (++), i.e.
+-- O(+)
+
+mapw1 = M.insertWith (\s a -> (head s):a) (ST.pack "Genus_A") [ST.pack "XAGT"] initMap
+
+hsTuples = L.map (\r -> (LT.toStrict $ header r, [LT.toStrict $ FastA.sequence r])) fastaRecs
+
+-- TODO: produce final map by folding hsTuples on initMap. Abstract the line
+-- with insertWith above into a function
+
+finalMap = L.foldl f initMap hsTuples
+
+f :: (M.Map SciName Alignment) -> (SciName, [Sequence]) -> (M.Map SciName Alignment)
+f map (otu, singleton) = M.insertWith g otu singleton map
+    where g new acc = (head new):acc
 
 
 -- To test in GHCi, e.g.:
