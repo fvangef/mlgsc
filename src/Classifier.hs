40c5f0bae59c5507bc1467cbb022cb9d113b8230
Thomas Junier <thomas.junier@epfl.ch>
2014-08-20 09:59:31 +0200

    more efficient (at least in theory...) function for building (otu -> aln) map

--- src/Classifier.hs
+++ src/Classifier.hs
@@ -1,7 +1,8 @@
 module Classifier where
 
 import qualified Data.Map.Strict as M -- most likely going to use all values
-import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text as ST
 import qualified Data.List as L
 
 import MlgscTypes
@@ -14,30 +15,30 @@ fastARecords2alnMap = undefined
 
 fasta = unlines [
     ">Genus_A",
-    "ATGCAT",
+    "AXGCAT",
     "GCGTGT",
     ">Genus_B",
-    "ATG",
+    "BXG",
     "CAT",
     "GCG",
     "TGC",
     ">Genus_A",
-    "ATGC",
+    "AYGC",
     "ATGC",
     "GTGC",
     ">Genus_C",
-    "XTGCATGCATGC",
+    "CXGCATGCATGC",
     ">Genus_B",
-    "GTGCATGCATAC",
+    "BYGCATGCATAC",
     ">Genus_C",
-    "YTGCATGCATGC",
+    "CYGCATGCATGC",
     ">Genus_C",
-    "NTGCTTGCATGC"
+    "CZGCTTGCATGC"
     ]
 
-fastaRecs = fastATextToRecords $ T.pack fasta
+fastaRecs = fastATextToRecords $ LT.pack fasta
 
-headers = map header fastaRecs
+headers = map (LT.toStrict . header) fastaRecs
 
 -- initial map, with one empty list per genus:
 
@@ -46,9 +47,15 @@ initMap = L.foldl (\map otu -> M.insert otu [] map) M.empty headers
 -- how to insert one element into the map, using Cons (:) instead of (++), i.e.
 -- O(+)
 
-mapw1 = M.insertWith (\s a -> (head s):a) (T.pack "Genus_A") [T.pack "XAGT"] initMap
+mapw1 = M.insertWith (\s a -> (head s):a) (ST.pack "Genus_A") [ST.pack "XAGT"] initMap
 
-hsTuples = L.map (\r -> (header r, [FastA.sequence r])) fastaRecs
+hsTuples = L.map (\r -> (LT.toStrict $ header r, [LT.toStrict $ FastA.sequence r])) fastaRecs
 
 -- TODO: produce final map by folding hsTuples on initMap. Abstract the line
 -- with insertWith above into a function
+
+finalMap = L.foldl f initMap hsTuples
+
+f :: (M.Map SciName Alignment) -> (SciName, [Sequence]) -> (M.Map SciName Alignment)
+f map (otu, singleton) = M.insertWith g otu singleton map
+    where g new acc = (head new):acc
