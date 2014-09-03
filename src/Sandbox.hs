1a263bf5e5a14c5a256010912c583fbac294690d
Thomas Junier <thomas.junier@epfl.ch>
2014-09-03 10:07:42 +0200

    more steps twrd Classifier building

--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -1,9 +1,12 @@
 import qualified Data.Text.Lazy as LT
+import qualified Data.Text as ST
 import qualified Data.Map.Strict as M
 import Data.Tree
 
 import MlgscTypes
 import NewickParser
+import CladeModel
+import NucModel
 import Classifier
 import FastA
 import Crumbs
@@ -64,3 +67,20 @@ treeOfAlns = mergeAlns treeOfLeafAlns
 -- TODO: covert treeOfAlns into a tree of NucModels, by fmapping the NucModel
 -- constructor. Them it should be possible to score a sequence by using
 -- dropCrumbs.
+
+treeOfNucModels = fmap (alnToNucModel 0.0001 1000) treeOfAlns
+
+scoreCrumbs :: (CladeModel mod) => Sequence -> mod -> Int
+scoreCrumbs seq mod = scoreSeq mod seq -- isn't this flip scoreSeq?
+
+-- Now score sequences according to the classifier, e.g.
+--
+(s1, c1) = dropCrumbs (scoreCrumbs $ ST.pack "ACGTACGT") treeOfNucModels 
+(s2, c2) = dropCrumbs (scoreCrumbs $ ST.pack "CCGTACGT") treeOfNucModels 
+(s3, c3) = dropCrumbs (scoreCrumbs $ ST.pack "CCGTACGG") treeOfNucModels 
+
+-- And recover the OTU name from the original tree:
+
+otu1 = followCrumbs c1 tree
+otu2 = followCrumbs c2 tree
+otu3 = followCrumbs c3 tree
