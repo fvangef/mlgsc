fbfafdec6374c058a0a4c10f10362231678066a3
Thomas Junier <thomas.junier@epfl.ch>
2014-09-10 10:56:20 +0200

    Does cross-validation with train and test sets each as half the input set

--- src/mlgsc_xval.hs
+++ src/mlgsc_xval.hs
@@ -10,6 +10,8 @@ import System.Environment (getArgs)
 --
 import qualified Data.Map.Strict as M
 import qualified Data.Set as S
+import Data.Sequence ((><), Seq)
+import qualified Data.Sequence as Sq
 import qualified Data.Text.Lazy.IO as LTIO
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text as ST
@@ -23,7 +25,8 @@ import MlgscTypes
 import FastA
 import Crumbs (dropCrumbs, followCrumbs)
 import NucModel
-import Classifier (NucClassifier, buildNucClassifier)
+import Classifier (NucClassifier, otuTree, 
+        buildNucClassifier, scoreSequenceWithCrumbs)
 import NewickParser
 import NewickDumper
 
@@ -45,27 +48,30 @@ main = do
     let classifier = buildNucClassifier smallProb scaleFactor otuAlnMap tree
     -- Remove OTUs from the test set if they're not in the tree - can't be used
     -- for testing
-    let testInTree = dropOTUsNotInTree tree testSet
     -- Also remove those not in train set - no chance of identifying them
     putStrLn "The following are retained"
-    let testInTrainSet = dropOTUsNotInTrainSet otuAlnMap testInTree
-    mapM_ (LTIO.putStrLn . header) testInTrainSet
+    let finalTestSet = (dropOTUsNotInTrainSet otuAlnMap .
+                           dropOTUsNotInTree tree) testSet
+    mapM_ (LTIO.putStrLn . header) finalTestSet
+    mapM_ (putStrLn . scoreQuery classifier) finalTestSet
+    
 
-
-dumpAlnMap :: OTUToAlnMap -> [String]
-dumpAlnMap otuAlnMap = map f $ M.assocs otuAlnMap
-    where f (k, v) = otu ++ " (" ++ num ++ " seq)"
-            where   otu = ST.unpack k
-                    num = show $ length v 
+scoreQuery :: NucClassifier -> FastA -> String
+scoreQuery classifier query =
+    (LT.unpack $ FastA.header query) ++ " -> " ++ (ST.unpack otu)
+    where   otu = followCrumbs crumbs $ otuTree classifier
+            (score, crumbs) = scoreSequenceWithCrumbs classifier querySeq 
+            querySeq = LT.toStrict $ FastA.sequence query
 
 -- Splits the FastA input into a training set and a test set (half and half).
 -- This should work unless two genera happen to occur mixed with each other,
 -- in perfect alternation. Not very likely, I surmise.
 
 splitAln :: [FastA] -> ([FastA], [FastA])
-splitAln (rec1:rec2:rest) = (rec1:rest1, rec2:rest2)
+splitAln (rec1:rec2:rest)   = (rec1:rest1, rec2:rest2)
     where (rest1, rest2) = splitAln rest
-    
+splitAln [rec]              = ([rec], [])
+splitAln  []                = ([], [])
 
 -- Takes a list of FastA records (meant to be the test set) drops any
 -- records whose OTU is not found in the tree.
@@ -78,3 +84,14 @@ dropOTUsNotInTree otuTree testFastARecs =
 dropOTUsNotInTrainSet :: OTUToAlnMap -> [FastA] -> [FastA]
 dropOTUsNotInTrainSet otuAlnMap testFastARecs =
     filter (\fasta -> M.member (LT.toStrict $ header fasta) otuAlnMap) testFastARecs
+
+-- Given any Seq, returns its nth element as well as all the others, in the
+-- same order. Note: this is a Data.Sequence.Seq, not a (biomolecular)
+-- Sequence. I just use a Seq instead of a list because it's faster.
+
+spliceElemAt :: Seq a -> Int -> (a, Seq a)
+spliceElemAt seq n = (elem, head >< tail)
+    where   (head, rest) = Sq.splitAt n seq
+            elem = Sq.index rest 0
+            tail = Sq.drop 1 rest
+
