7526ee8ca6c69ee43bc2048771ff17830f70f42c
Thomas Junier <thomas.junier@epfl.ch>
2014-10-06 13:47:51 +0200

    Weights: -> use of new wgt matrix

--- src/Weights.hs
+++ src/Weights.hs
@@ -23,34 +23,14 @@ normalize rawWts = L.map (round . (/ (minimum rawWts))) rawWts
 -- alignment.
 
 alnRawWeights :: Alignment -> [Double]
-alnRawWeights aln =
-    L.map sum $ L.transpose $ L.map resColToWeightCol $ T.transpose $
-        L.map Alignment.rowSeq aln
+alnRawWeights aln = L.Map (henikoffWeightRow hMat) aln
+    where hMat = henikoffMatrix aln
 
--- the number of _different_ residues in the column, and s is the number of
--- sequences which have residue R in that column. For example, for a column
--- consisting of G, G, and C the value of n is 2 (two different residues, G and
--- C). The value of n is 2 for G and 1 for C, as there are two Gs and one C in
--- the column. The weight of G at that column is therefore 1 / (2 * 2) = 1/4,
--- and that for C is 1 / (2 * 1) = 1/2.
+henikoffWeightRow :: HenikoffMatrix -> ALnRow -> Double  
+henikoffWeightRow hMat (AlnRow _ seq _) = henikoffWeightSeq hMat seq
 
-resWeight :: Int -> Int -> Double
-resWeight num_res num_seq_with_res = 1.0 / (n * s)
-    where   s = fromIntegral num_seq_with_res
-            n = fromIntegral num_res 
+henikoffWeightSeq :: HenikoffMatrix -> Sequence -> Double
+henikoffWeightSeq = undefined
 
--- Constructs the (residue -> weight) map for a given column. The keys are the
--- different residues in the column, e.g. for a column consisting of G, G, C the
--- map would have a key for G (with value 1/4) and a key for C (with value 1/2).
 
-colToWeightsMap :: Column -> M.Map Char Double
-colToWeightsMap col = M.map (resWeight $ M.size res_count_map) res_count_map
-    where res_count_map = M.fromListWith (+) $ zip (T.unpack col) $ repeat 1
-
--- Maps all residues in a column to their weight, e.g. ['G', 'G', 'C'] -> [1/4,
--- 1/4, 1/2].
-
-resColToWeightCol :: Column -> [Double]
-resColToWeightCol col = L.map ((M.!) weightsMap) (T.unpack col)
-    where weightsMap = colToWeightsMap col
 
