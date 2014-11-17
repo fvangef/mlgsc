18429dbad0edda93a88e5732280b53b6e22a2825
Thomas Junier <thomas.junier@epfl.ch>
2014-11-17 22:03:58 +0100

    shows whole taxonomy instead of just OTU

--- src/mlgsc.hs
+++ src/mlgsc.hs
@@ -8,8 +8,9 @@ import System.Environment (getArgs)
 --import System.Console.GetOpt
 --import System.IO
 --
-import qualified Data.Text.Lazy.IO as LTIO
 import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.IO as LTIO
+import qualified Data.Text as ST
 import qualified Data.Text.IO as STIO
 
 import Data.Binary (decodeFile)
@@ -18,7 +19,7 @@ import Data.Tree
 --
 import MlgscTypes
 import FastA
-import Crumbs (dropCrumbs, followCrumbs)
+import Crumbs (dropCrumbs, followCrumbs, followCrumbsWithTrail)
 import NucModel
 import Classifier (NucClassifier, otuTree, modTree, scoreSequenceWithCrumbs)
 
@@ -33,10 +34,15 @@ main = do
     let queryRecs = fastATextToRecords queryFastA
     classifier <- (decodeFile classifierFname) :: IO NucClassifier
     let query = LT.toStrict $ FastA.sequence $ queryRecs !! 2
-    let otu = classifySequence classifier query
+    let otu = classifySequenceWithTrail classifier query
     STIO.putStrLn otu
 
 -- classifySequence :: NucClassifier -> Sequence -> LT.Text
 classifySequence classifier query = otu
     where   (score, crumbs) = scoreSequenceWithCrumbs classifier query  
             otu = followCrumbs crumbs $ otuTree classifier
+
+-- classifySequence :: NucClassifier -> Sequence -> LT.Text
+classifySequenceWithTrail classifier query = taxo
+    where   (score, crumbs) = scoreSequenceWithCrumbs classifier query  
+            taxo = ST.intercalate (ST.pack "; ") $ followCrumbsWithTrail crumbs $ otuTree classifier
