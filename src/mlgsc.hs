15db61162f46083d701d23486d71a5a1e8c4afb1
Thomas Junier <thomas.junier@epfl.ch>
2014-12-17 14:53:50 +0100

    + option -A to prevent alignment

--- src/mlgsc.hs
+++ src/mlgsc.hs
@@ -10,6 +10,7 @@ import qualified Data.Text.Lazy.IO as LTIO
 import qualified Data.Text as ST
 import qualified Data.Text.IO as STIO
 import Text.Printf
+import Options.Applicative
 
 import Data.Binary (decodeFile)
 import Data.Tree
@@ -24,17 +25,42 @@ import Classifier (NucClassifier, otuTree, modTree, scoreSequenceWithCrumbs,
         scoreSequenceWithExtendedCrumbs)
 import Output
 
+data Params = Params {
+                optNoAlign          :: Bool
+                , queryFname        :: String
+                , clsfrFname        :: String
+                }
+
+parseOptions :: Parser Params
+parseOptions = Params
+                <$> switch
+                    (long "no-align"
+                    <> short 'A'
+                    <> help "do not align query sequences")
+                <*> argument str (metavar "<query seq file>")
+                <*> argument str (metavar "<classifier file>")
+
+parseOptionsInfo :: ParserInfo Params
+parseOptionsInfo = info (helper <*> parseOptions) 
+                    ( fullDesc
+                    <> progDesc "classify sequences according to a model"
+                    <> Options.Applicative.header
+                        "mlgsc - maximum-likelihood general sequence classifier")
+
 main :: IO ()
 main = do
-    [queriesFname, classifierFname] <- getArgs   
-    queryFastA <- LTIO.readFile queriesFname
+    params <- execParser parseOptionsInfo
+    queryFastA <- LTIO.readFile $ queryFname params
     let queryRecs = fastATextToRecords queryFastA
-    classifier <- (decodeFile classifierFname) :: IO NucClassifier
+    classifier <- (decodeFile $ clsfrFname params) :: IO NucClassifier
     let rootMod = rootLabel $ modTree classifier 
     let scoringScheme = ScoringScheme (-2) (scoringSchemeMap (absentResScore rootMod))
+    let processQuery = if (optNoAlign params)
+                            then id
+                            else (msalign scoringScheme rootMod)
     let headers = map FastA.header queryRecs
     let predictions = map (classifySequenceWithExtendedTrail classifier .
-                            (msalign scoringScheme rootMod) .
+                            processQuery .
                             LT.toStrict . 
                             FastA.sequence) queryRecs
     mapM_ STIO.putStrLn $ zipWith output headers predictions
