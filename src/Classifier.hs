77f63645f209c8b439db8976620a0bc383f4dd48
Thomas Junier <thomas.junier@epfl.ch>
2015-04-14 23:48:42 +0200

    moved ER functions to Classifier.hs

--- src/Classifier.hs
+++ src/Classifier.hs
@@ -135,3 +135,34 @@ mergeNamedAlns (Node (name,_) kids) = Node (name,mergedKidAlns) mergedKids
 
 leafOTU :: Trail -> OTUName
 leafOTU trail = otuName $ last trail
+
+-- Computes the base-10 log of the evidence ratio, i.e. log_10 (exp(delta-AIC /
+-- 2)), except that I use delta-AIC' (in which the factor 2 is dropped, so I
+-- avoid having to multiply by 2 only to divide by 2 again just after).
+
+log10evidenceRatio :: Int -> Int -> Int -> Double
+log10evidenceRatio scaleFactor bestScore secondBestScore = logBase 10 er
+    where   l_min = scoreTologLikelihood scaleFactor bestScore
+            l_sec = scoreTologLikelihood scaleFactor secondBestScore
+            er = exp(deltaAIC' l_min l_sec) 
+
+-- Converts a model score (which is a scaled, rounded log-likelihood (log base
+-- 10)) to a log-likelihood (log base e, i.e. ln). To do this, we _divide_ by
+-- the scale factor to get an unscaled log10-likelihood, and then divide by
+-- log10(e) to get a ln-based likelihood.
+
+scoreTologLikelihood :: Int -> Int -> Double
+scoreTologLikelihood scaleFactor score = log10Likelihood / logBase 10 e
+    where   log10Likelihood = fromIntegral score / fromIntegral scaleFactor
+            e = exp(1.0)
+            
+-- Computes the difference in AIC of two log-likelihoods, taking into account
+-- that the number of parameters k is in our case the same in any two models,
+-- and this cancels out, i.e. delta AIC = AIC1 - AIC2 = 2k -2 ln (L_1) - (2k -
+-- 2 ln(L_2)) = -2 (ln (L_1) - ln (L_2)). Since the arguments are already _log_
+-- likelihoods, the expression simplifies to -2 (l_1 - l_2), where l_1 =
+-- ln(L_1), etc. I also drop the constant 2, since we'd be dividing by 2 right
+-- away in evidenceRatio anyway.
+
+deltaAIC' :: Double -> Double -> Double
+deltaAIC' l1 l2 = - (l1 - l2) -- or simply l2 - l1...
