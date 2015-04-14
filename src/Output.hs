4d40951939e84282bbf36ec51ea05ef2a291ab8c
Thomas Junier <thomas.junier@epfl.ch>
2015-04-14 23:39:24 +0200

    moved all output-related f() to Output.hs

--- src/Output.hs
+++ src/Output.hs
@@ -1,22 +1,18 @@
 module Output where
 
--- TODO: restrict exports to only those functions that are needed by callers.
-
-import Control.Applicative
-
-import qualified Data.Text.Lazy as LT
-import qualified Data.Text.Lazy.IO as LTIO
-import qualified Data.Text as ST
-import qualified Data.Text.IO as STIO
-import Text.Printf
+-- TODO: check imports
 
 import Data.Binary (decodeFile)
 import Data.Tree
---import System.Directory
---
+import qualified Data.Text as ST
+import qualified Data.Text.IO as STIO
+import qualified Data.Text.Lazy as LT
+import qualified Data.Text.Lazy.IO as LTIO
+import Text.ParserCombinators.Parsec
+import Text.Printf
+
 import MlgscTypes
 import FastA
-import OutputFormatStringParser
 
 -- formats an output line according to a format string (à la printf). ARguments
 -- are: format string, original query as a FastA record, query sequence after
@@ -26,108 +22,138 @@ import OutputFormatStringParser
 -- query.
 
 -- TODO: pass only the argments that vary from query to query, and the rest
--- using a Reader monad.
--- TODO: could add a high-level way of passing an ER threshold
+-- using a Reader monad. See  formatResultReader in mlgsc.hs
 
-formatResult :: FmtString -> FastA -> Sequence -> Trail
+formatResult :: FmtString -> StepFmtString -> FastA -> Sequence -> Trail
     -> ST.Text
-formatResult fmtString query alnQry trail = 
-    ST.concat $ map (evalFmtComponent query alnQry trail) format
-        where (Right format) = parseOutputFormatString fmtString
+formatResult fmtString stepFmtString query alnQry trail = 
+    ST.concat $ map (evalFmtComponent query alnQry trail stepfmt) format
+        where   (Right format) = parseOutputFormatString fmtString
+                (Right stepfmt) = parseStepFormatString stepFmtString
 
-{- This function should NOT take parameters as a record, or as a data type, or
- - from a Reader, because it is used from different programs (at least mlgsc and
- - mlgsc_xval) which have their own types for params, etc. Accordingly, all
- - parameters to this function are explicit. It is possible, however, to call it
- - from Reader functions (e.g. in mlgsc). -}
+
+data FmtComponent = Literal Char
+                    | ID
+                    | Header
+                    | Path
+                    | UPath     -- path with "unclassified"
+                    | QueryLength
+                    | Query
+                    | AlignedQuery
+                    | Score
+                    deriving (Show)
+                       
+type Format = [FmtComponent]
+type FmtString = String
+
+literalChar :: Parser FmtComponent
+literalChar = do
+    c <- noneOf "%"
+    return $ Literal c
+
+escape :: Parser FmtComponent
+escape = do
+    char '%'
+    f <- oneOf "%ahilpqsu"
+    return $ case f of
+                '%' -> Literal '%'
+                'a' -> AlignedQuery
+                'l' -> QueryLength
+                'h' -> Header
+                'i' -> ID
+                'p' -> Path
+                'q' -> Query
+                's' -> Score
+                'u' -> UPath
+
+fmtComponent :: Parser FmtComponent
+fmtComponent = literalChar <|> escape
+
+format :: Parser [FmtComponent]
+format = many fmtComponent
+
+parseOutputFormatString :: String -> Either ParseError (Format)
+parseOutputFormatString fmt = (parse format "format" fmt)
 
 evalFmtComponent :: FastA
                  -> Sequence
                  -> Trail
+                 -> StepFormat
                  -> FmtComponent
                  -> ST.Text
-evalFmtComponent query alnQry trail component = case component of
+evalFmtComponent query alnQry trail stepfmt component = case component of
     Header          -> LT.toStrict $ FastA.header query
     QueryLength     -> ST.pack $ show $ LT.length $ FastA.sequence query
     ID              -> LT.toStrict $ fastAId query
     Query           -> LT.toStrict $ FastA.sequence query
     AlignedQuery    -> alnQry
-    Path            -> trailToPath trail 
-    UPath           -> trailToUPath trail 
+    Path            -> trailToPath stepfmt trail 
+    UPath           -> trailToUPath stepfmt trail 
     Score           -> ST.pack $ show $ bestScore $ last trail
     (Literal c)     -> ST.pack [c]
 
+trailToPath :: StepFormat -> Trail -> ST.Text
+trailToPath stepfmt trail = ST.intercalate (ST.pack "; ") $
+                    map (formatStep stepfmt) trail
 
-trailToPath :: Trail -> ST.Text
-trailToPath trail = ST.intercalate (ST.pack "; ") $ map stepToText trail
-    where stepToText step = otuName step
-                                
-{-
-trailToPath' :: Trail -> ST.Text
-trailToPath' trail =
-    if ((length $ getZipList good_log10ers) ==
-        (length $ getZipList log10ers))
-        then path
-        else path `ST.append` (ST.pack "; unclassified")
-    where   path = ST.intercalate (ST.pack "; ") $ getZipList erLbls
-            labels  = ZipList $ map otuName trail
-            bests   = ZipList $ map bestScore trail
-            seconds = ZipList $ map secondBestScore trail
-            log10ers = log10evidenceRatio <$>
-                       (ZipList $ repeat 1000) <*> seconds <*> bests
-            good_log10ers = cutAtFirstPoorER min_er log10ers
-            erLbls = toERlbl <$> labels <*> log10ers
-            toERlbl lbl er = ST.concat [lblOrUndef,
-                                 ST.pack " (", 
-                                 ST.pack erStr,
-                                 ST.pack ")"]
-                where erStr = case printf "%.0g" er :: String of
-                            "Infinity" -> "*"
-                            otherwise -> printf "%.0g" er :: String
-                      lblOrUndef = if ST.empty == lbl
-                                        then ST.pack "unnamed"
-                                        else lbl
--}
+trailToUPath = undefined
 
-trailToUPath :: Trail -> ST.Text
-trailToUPath x = undefined
 
--- Computes the base-10 log of the evidence ratio, i.e. log_10 (exp(delta-AIC /
--- 2)), except that I use delta-AIC' (in which the factor 2 is dropped, so I
--- avoid having to multiply by 2 only to divide by 2 again just after).
 
-log10evidenceRatio :: Int -> Int -> Int -> Double
-log10evidenceRatio scaleFactor bestScore secondBestScore = logBase 10 er
-    where   l_min = scoreTologLikelihood scaleFactor bestScore
-            l_sec = scoreTologLikelihood scaleFactor secondBestScore
-            er = exp(deltaAIC' l_min l_sec) 
+data StepFmtComponent = SLiteral Char   -- 'Literal' already taken...
+                        | TaxonName
+                        | SupportVal
+                        | BestScore
+                        deriving (Show)
 
--- Converts a model score (which is a scaled, rounded log-likelihood (log base
--- 10)) to a log-likelihood (log base e, i.e. ln). To do this, we _divide_ by
--- the scale factor to get an unscaled log10-likelihood, and then divide by
--- log10(e) to get a ln-based likelihood.
+type StepFormat = [StepFmtComponent]
+type StepFmtString = String
 
-scoreTologLikelihood :: Int -> Int -> Double
-scoreTologLikelihood scaleFactor score = log10Likelihood / logBase 10 e
-    where   log10Likelihood = fromIntegral score / fromIntegral scaleFactor
-            e = exp(1.0)
-            
--- Computes the difference in AIC of two log-likelihoods, taking into account
--- that the number of parameters k is in our case the same in any two models,
--- and this cancels out, i.e. delta AIC = AIC1 - AIC2 = 2k -2 ln (L_1) - (2k -
--- 2 ln(L_2)) = -2 (ln (L_1) - ln (L_2)). Since the arguments are already _log_
--- likelihoods, the expression simplifies to -2 (l_1 - l_2), where l_1 =
--- ln(L_1), etc. I also drop the constant 2, since we'd be dividing by 2 right
--- away in evidenceRatio anyway.
+-- A Literal character, for a Step component (literalChar is already used for
+-- output format components)
 
--- TODO: deltaAIC' -> deltaAIC
-deltaAIC' :: Double -> Double -> Double
-deltaAIC' l1 l2 = - (l1 - l2) -- or simply l2 - l1...
+sLiteralChar :: Parser StepFmtComponent
+sLiteralChar = do
+    c <- noneOf "%"
+    return $ SLiteral c
 
--- Takes a threshold and a ZipList of evidence ratios and drops any and all
--- after the first below the threshold. The idea is to keep those nodes in the
--- path that are well enough supprted, but to drop anything beyond (and
--- including) the first poorly-supported node.
+-- Same idea for escape
 
-cutAtFirstPoorER :: Int -> ZipList Double -> ZipList Double
-cutAtFirstPoorER min_er ers = ZipList $ takeWhile (>= fromIntegral min_er) $ getZipList ers 
+sEscape :: Parser StepFmtComponent
+sEscape = do
+    char '%'
+    f <- oneOf "%bst"
+    return $ case f of
+                '%' -> SLiteral '%'
+                't' -> TaxonName
+                's' -> SupportVal
+                'b' -> BestScore
+
+stepFmtComponent :: Parser StepFmtComponent
+stepFmtComponent = sLiteralChar <|> sEscape
+
+stepFormat :: Parser StepFormat
+stepFormat = many stepFmtComponent
+
+
+parseStepFormatString :: String -> Either ParseError (StepFormat)
+parseStepFormatString sfmt = (parse stepFormat "stepformat" sfmt)
+
+run :: Show a => Parser a -> String -> IO ()
+run p input 
+	= case (parse p "" input) of
+		Left err -> do 	{ putStr "parse error at "
+				; print err
+				}
+		Right x	-> print x
+
+formatStep :: StepFormat -> Step -> ST.Text
+formatStep stepFormat step =
+    ST.concat $ map (evalStepFmtComponent step) stepFormat
+
+evalStepFmtComponent :: Step -> StepFmtComponent -> ST.Text
+evalStepFmtComponent step component = case component of
+    (SLiteral char) -> ST.pack [char]
+    TaxonName       -> otuName step
+    SupportVal      -> ST.pack $ show $ log10ER step
+    BestScore       -> ST.pack $ show $ bestScore step
