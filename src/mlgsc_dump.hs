d99ef801fd86d92db4cf4c13482da8f6a325f02b
Thomas Junier <thomas.junier@epfl.ch>
2015-08-12 11:14:56 +0200

    mlgsc_dump shows taxon tree

--- src/mlgsc_dump.hs
+++ src/mlgsc_dump.hs
@@ -6,37 +6,23 @@
 
 import System.Environment (getArgs)
 
---import qualified Data.Text.Lazy.IO as LTIO
---import qualified Data.Text.Lazy as LT
 import qualified Data.Text.IO as STIO
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as U
 
 import Data.Binary (decodeFile)
 import Data.Tree
---import System.Directory
 --
---import MlgscTypes
---import Crumbs (dropCrumbs, followCrumbs)
-import NucModel
-import Classifier (NucClassifier, otuTree, modTree)
+import MlgscTypes
+import PWMModel (cladeName)
+import Classifier (Classifier(..), classifySequence)
 import NewickDumper
-
---import Trees
---import Align
---import Cdf
 
 main :: IO ()
 main = do
-    [classifierFname] <- getArgs   
-    classifier <- (decodeFile classifierFname) :: IO NucClassifier
-    let otutree = otuTree classifier
-    let modtree = modTree classifier
-    STIO.putStrLn $ treeToNewick otutree 
-    putStrLn $ drawTree $ fmap show otutree
-    putStrLn $ drawTree $ fmap mod2String modtree
+    [clsfrFname] <- getArgs   
+    classifier@(PWMClassifier modTree scale) <- (decodeFile $ clsfrFname) :: IO Classifier
+    let taxonTree = fmap cladeName modTree 
+    putStrLn "Taxon tree:"
+    STIO.putStrLn $ treeToNewick taxonTree 
     return ()
-
-mod2String :: NucModel -> String
-mod2String mod = show $ U.length $ V.head $ matrix mod
-
