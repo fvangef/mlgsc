ff8b8615f2817e0e961e3d173a888b372c42125d
Thomas Junier <thomas.junier@epfl.ch>
2015-09-09 17:16:44 +0200

    proof of concept of HMM for queries shorter than model

--- src/hmmtoy.hs
+++ src/hmmtoy.hs
@@ -1,31 +1,50 @@
-import Data.HMM
-import Control.Monad
-import Data.Array
-import System.IO
+import Learning.HMM
+import Data.Random.Distribution.Categorical as C
 
-hmm1 = HMM  { states = [1,2]
-            , events = ['A', 'G', 'C', 'T']
-            , initProbs = ip
-            , transMatrix = tm
-            , outMatrix = om
-            }
+import Data.Map ((!))
+import Data.Map as M
 
-ip s
-   | s == 1 = 0.1
-   | s == 2 = 0.9
+data State = Pre | Aln | Post
+  deriving (Show, Ord, Eq)
 
-tm s1 s2
-   | s1 == 1 && s2 == 1 = 0.9
-   | s1 == 1 && s2 == 2 = 0.1
-   | s1 == 2 && s2 == 1 = 0.5
-   | s1 == 2 && s2 == 2 = 0.5
+type Path = [State]
 
-om s e
-   | s == 1 && e == 'A' = 0.4
-   | s == 1 && e == 'C' = 0.1
-   | s == 1 && e == 'G' = 0.1
-   | s == 1 && e == 'T' = 0.4
-   | s == 2 && e == 'A' = 0.1
-   | s == 2 && e == 'C' = 0.4
-   | s == 2 && e == 'G' = 0.4
-   | s == 2 && e == 'T' = 0.1
+data Output = Gap | Residue
+  deriving (Show, Eq)
+
+hmmStates = [Pre, Aln, Post]
+
+hmmOutputs = [Gap, Residue]
+
+initProb = C.fromList [(0.9, Pre), (0.1, Aln), (0.0, Post)]
+
+transProb :: State -> Categorical Double State
+transProb Pre = C.fromList [(0.99, Pre), (0.01, Aln), (0.0, Post)]
+transProb Aln = C.fromList [(0.0, Pre), (0.99, Aln), (0.01, Post)]
+transProb Post = C.fromList [(0.0, Pre), (0.0, Aln), (1.0, Post)]
+
+emProb Pre = C.fromList [(0.95, Gap), (0.05, Residue)]
+emProb Aln = C.fromList [(0.05, Gap), (0.95, Residue)]
+emProb Post = C.fromList [(0.95, Gap), (0.05, Residue)]
+
+hmm = HMM hmmStates hmmOutputs initProb transProb emProb
+
+alnSeqToHMMSym :: String -> [Output]
+alnSeqToHMMSym = Prelude.map (\c -> if c == '-' then Gap else Residue) 
+
+transitionsPos :: Path -> (Int, Int)
+transitionsPos path = (nbPre, nbPre + nbAln)
+    where counts = M.fromListWith (+) $ zip path (repeat 1) :: Map State Int
+          nbPre = counts ! Pre
+          nbAln = counts ! Aln
+
+boundaries :: HMM State Output -> String -> (Int, Int)
+boundaries hmm seq = transitionsPos path
+    where (path, _) = viterbi hmm $ alnSeqToHMMSym seq
+
+seq1 = [Gap, Gap, Gap, Gap, Gap, Gap, Gap, Gap, Gap, Gap, Gap, Gap, Gap, Gap, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Residue, Gap, Gap, Gap, Gap, Gap, Gap, Gap, Gap, Gap]
+
+alnSeq1 = "-------------cgacgagcgacttacgagcagcgatg-------------"
+
+(outs, _) = viterbi hmm $ alnSeqToHMMSym alnSeq1
+
