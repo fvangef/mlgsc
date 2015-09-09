c640046fc15ab52d9de6c5792e48dc2db10d5713
Thomas Junier <thomas.junier@epfl.ch>
2015-09-09 17:44:17 +0200

    tests for ID-tree

--- test/test_mlgsc_func.sh
+++ test/test_mlgsc_func.sh
@@ -9,4 +9,8 @@ function test_init
 		echo "re-building idaln classifier (binary is newer)"
 		$BINDIR/mlgsc_train -i Prot idaln.mfa idtree.nw
 	fi
+	if [ $BINDIR/mlgsc_train -nt frc_4train.bcls ] ; then
+		echo "re-building frc taxo classifier (binary is newer)"
+		$BINDIR/mlgsc_train -T Prot frc_4train.msa bacteria.taxo
+	fi
 }
