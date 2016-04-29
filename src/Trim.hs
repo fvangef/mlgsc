b51c880b1b70098bba32dd6687fbd7cf7f87e879
Thomas Junier <thomas.junier@epfl.ch>
2016-04-29 14:33:22 +0200

    ready to add HMM for aln with gapped regions

--- src/Trim.hs
+++ src/Trim.hs
@@ -20,31 +20,51 @@ type Path = [State]
 data Output = Gap | Residue
   deriving (Show, Eq)
 
-hmmStates = [Pre, Aln, Post]
+-- A simple HMM that models the alignment of a short sequence to a
+-- "good" alignment (i.e., with no (or only few) unaligned blocks). This assumes
+-- the short sequence has a well-aligned part (Aln state in the model),
+-- possibly preceded by a leading gapped region (missing leading sequence,
+-- Pre state in the model), and possibly followed by a trailing gapped region
+-- (missing trailing sequence, Post state in the model).
 
-hmmOutputs = [Gap, Residue]
+trimHmmStates = [Pre, Aln, Post]
 
-initProb = C.fromList [(0.9, Pre), (0.1, Aln), (0.0, Post)]
+trimHmmOutputs = [Gap, Residue]
 
-transProb :: State -> Categorical Double State
-transProb Pre = C.fromList [(0.99, Pre), (0.01, Aln), (0.0, Post)]
-transProb Aln = C.fromList [(0.0, Pre), (0.99, Aln), (0.01, Post)]
-transProb Post = C.fromList [(0.0, Pre), (0.0, Aln), (1.0, Post)]
+trimHmmInitProb = C.fromList [(0.9, Pre), (0.1, Aln), (0.0, Post)]
 
-emProb Pre = C.fromList [(0.95, Gap), (0.05, Residue)]
-emProb Aln = C.fromList [(0.05, Gap), (0.95, Residue)]
-emProb Post = C.fromList [(0.95, Gap), (0.05, Residue)]
+trimHmmTransProb :: State -> Categorical Double State
+trimHmmTransProb Pre = C.fromList [(0.99, Pre), (0.01, Aln), (0.0, Post)]
+trimHmmTransProb Aln = C.fromList [(0.0, Pre), (0.99, Aln), (0.01, Post)]
+trimHmmTransProb Post = C.fromList [(0.0, Pre), (0.0, Aln), (1.0, Post)]
 
-hmm = HMM hmmStates hmmOutputs initProb transProb emProb
+trimHmmEmProb Pre = C.fromList [(0.95, Gap), (0.05, Residue)]
+trimHmmEmProb Aln = C.fromList [(0.05, Gap), (0.95, Residue)]
+trimHmmEmProb Post = C.fromList [(0.95, Gap), (0.05, Residue)]
 
+trimHmm = HMM trimHmmStates trimHmmOutputs
+    trimHmmInitProb trimHmmTransProb trimHmmEmProb
+
+-- trimSeq should be replaced by trimSeqGeneric trimHmm
 trimSeq :: Text -> Text
 trimSeq seq = ST.pack $ zipWith translate path seqAsStr
-    where   (path, _)   = viterbi hmm $ alnSeqToHMMSym seqAsStr
+    where   (path, _)   = viterbi trimHmm $ alnSeqToHMMSym seqAsStr
             seqAsStr    = ST.unpack seq    
-            translate state c = case state of
-                                    Pre -> '.'
-                                    Aln -> c
-                                    Post -> '.'
+
+translate :: State -> Char -> Char
+translate state c = case state of
+                        Pre -> '.'
+                        Aln -> c
+                        Post -> '.'
+
+-- A generalized version of trimSeq, works for any HMM. I don't change existing
+-- names so as not to break code, but trimSeq should be the name of this
+-- function.
+
+trimSeqGeneric :: HMM State Output -> Text -> Text
+trimSeqGeneric hmm seq = ST.pack $ zipWith translate path seqAsStr
+    where   (path, _)   = viterbi hmm $ alnSeqToHMMSym seqAsStr
+            seqAsStr    = ST.unpack seq
 
 alnSeqToHMMSym :: String -> [Output]
 alnSeqToHMMSym = Prelude.map (\c -> if c == '-' then Gap else Residue) 
